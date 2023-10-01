7a96b0602d84fdfbbc037d78f29c31c9d07dafb5
Maryan Mats <cholgyni@gmail.com>
2023-10-01 09:54:23 +0300

    fix some things

--- src/components/AppContainer/AppContainer.tsx
+++ src/components/AppContainer/AppContainer.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { Breadcrumbs, Container } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Container } from '@mui/material';
 
 type Props = {
   children: React.ReactNode;
--- src/components/PostsTable/PostsTable.tsx
+++ src/components/PostsTable/PostsTable.tsx
@@ -1,54 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
 import { Post } from '../../types/types';
 import { useParams } from 'react-router-dom';
-import { SearchInput } from '../SearchInput/SearchInput';
+import { GenericTable } from '../GenericTable';
+import { fetchData } from '../../utils/fetchData';
 
 export const PostsDialog: React.FC = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [searchQuery, setSearchQuery] = useState('');
   const { userId } = useParams();
+  const headers = ['ID', 'TITLE', 'BODY'];
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
-      .then(response => response.json())
-      .then(json => setPosts(json))
+    fetchData(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
+      .then((json) => setPosts(json))
+      .catch((error) => {
+        console.log(error);
+      });
   }, [])
 
-  const filteredPosts = posts.filter(post =>
-    post.title.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
   return (
-    <>
-    <SearchInput value={searchQuery} setSearchQuery={setSearchQuery} />
-    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell><strong>ID</strong></TableCell>
-            <TableCell align="right"><strong>TITLE</strong></TableCell>
-            <TableCell align="right"><strong>BODY</strong></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {filteredPosts.map((post) => (
-            <TableRow
-              key={post.title}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {post.id}
-              </TableCell>
-              <TableCell align="right">{post.title}</TableCell>
-              <TableCell align="right">{post.body}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      </>
+    <GenericTable
+      data={posts}
+      headers={headers}
+      searchQuery={searchQuery}
+      setSearchQuery={setSearchQuery}
+    />
   );
-}
+};
